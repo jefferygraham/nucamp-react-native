094c61f1948185f46be3662230d6b68dc9ddf3a8
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-10-26 16:54:21 -0400

    Animatable

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,6 +4,7 @@ import { ListItem } from 'react-native-elements';
 import { SwipeRow } from 'react-native-swipe-list-view';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
+import * as Animatable from 'react-native-animatable';
 
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
@@ -80,13 +81,15 @@ class Favorites extends Component {
       );
     }
     return (
-      <FlatList
-        data={this.props.campsites.campsites.filter((campsite) =>
-          this.props.favorites.includes(campsite.id)
-        )}
-        renderItem={renderFavoriteItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <Animatable.View animation='fadeInRightBig' duration={2000}>
+        <FlatList
+          data={this.props.campsites.campsites.filter((campsite) =>
+            this.props.favorites.includes(campsite.id)
+          )}
+          renderItem={renderFavoriteItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Animatable.View>
     );
   }
 }
